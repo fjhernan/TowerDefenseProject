ea2b4c0e8240683e1c6eaa8a58f46739c1e7d5db
fjhernan <60201738+fjhernan@users.noreply.github.com>
2020-10-24 18:34:20 -0700

    Finished part 1a
    Finished the requirements for the 1st part of tower defense

---
+++ TowerDefense/Assets/Scripts/Enemy_1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Enemy_1 : MonoBehaviour
+{
+    private int health = 3;
+    private Transform waypoint;
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0){
+            Death();
+        }
+    }
+
+    public void Hurt(){
+        health--;
+        Debug.Log(health);
+    }
+
+    public void Movement(float movement){
+        transform.position = Vector3.MoveTowards(transform.position, waypoint.position, movement * Time.deltaTime);
+    }
+
+    public bool Distance()
+    {
+        if (Vector3.Distance(transform.position, waypoint.position) <= 0.1f)
+            return true;
+
+        return false;
+    }
+    public void SetWaypoint(Transform wp){
+        waypoint = wp;
+    }
+
+    private void Death(){
+        GameObject.Find("EnemyManager").GetComponent<EnemyManager>().EnemyDestroyed();
+        Destroy(gameObject);
+    }
+}
---
+++ TowerDefense/Assets/Scripts/PurseManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PurseManager : MonoBehaviour
+{
+    public TextMeshProUGUI purse;
+    private int coins = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        purse.text = "Coins: " + coins.ToString("D2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        purse.text = "Coins: " + coins.ToString("D2");
+    }
+
+    public void EarnCoins(int earn){
+        coins += earn;
+    }
+
+    public void Buying(int spent){
+        coins -= spent;
+    }
+
+    public int GetCoins(){
+        int temp = coins;
+        return temp;
+    }
+}
